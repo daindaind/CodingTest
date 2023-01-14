7a0e3d28bb9512fc4365ca6a73aafdce6524e918
dain cha <cdin23@naver.com>
2023-01-14 13:28:37 +0900

    second commit

--- bck_11401.py
+++ bck_11401.py
@@ -1,22 +1,57 @@
 from sys import stdin
 input = stdin.readline
 
-n, k = map(int, input().split())
+N, K =map(int, input().split())
+mod = 10**9 + 7
 
-def Power(n, k):    # 3  5
+def Fectorial(number):
+    num = 1
+    for i in range(1, number+1):
+        num *= i
+        num %= mod
+    return num
+
+def Power(n, k):
     if (k==1):
         return n
+    
     tmp = Power(n, k//2)
-    if (k%2 == 0):
-        return tmp*tmp
+    if k%2: 
+        return tmp * tmp * n % mod
     else:
-        return n*tmp*tmp
+        return tmp * tmp % mod
 
-print(Power(n, k))
+print(Fectorial(N)%mod * (Power(Fectorial(K)*Fectorial(N-K), mod-2))%mod)
 
- 
+# from sys import stdin
+# input = stdin.readline
 
+# def Fectorial(number):
+#     fct = 1
+#     for i in range(1, number+1):
+#         fct *= i
+#     return fct
 
+# def Power(n, k):
+#     if (k==1):
+#         return n
+#     elif (k==0):
+#         return 1
+
+#     tmp = Power(n, k//2)
+#     if (k%2 == 0):
+#         return tmp*tmp
+#     else:
+#         return n*tmp*tmp
+
+# n, k = map(int, input().split())
+# b = 10**9 + 7
+
+# top = Fectorial(n)
+# a = Fectorial(n-k) * Fectorial(k)
+
+# # a : Fectorial(n-k) * Fectorial(k)
+# print(Power(a, b-2)%b * top)
 
 
 #---------------------------------------
