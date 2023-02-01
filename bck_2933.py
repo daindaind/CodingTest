48eb4e141678e8c32a522b082e8495ea723d9b30
dain cha <cdin23@naver.com>
2023-02-01 21:30:48 +0900

    4th commit

--- bck_2933.py
+++ bck_2933.py
@@ -93,14 +93,6 @@ def throwStickRight(h):
             return h, i
     return -1, -1
 
-# def findSearch(target_x, target_y):
-#     #방문 표시 위치 리스트
-#     visited =  [[False]*c for _ in range(r)]
-#     visited[target_x][target_y] = 1
-#     q = deque([(target_x, target_y)])
-#     cave[target_x][target_y] = '.'
-
-
 #클러스터 방문 탐색하기
 def clusterVisited(start_y, start_x):   
     visited = [[False]*c for _ in range(r)]   #기록할 리스트
@@ -116,12 +108,13 @@ def clusterVisited(start_y, start_x):
 
             if not (0 <= ny < r and 0 <= nx < c):  #탐색하려는 부분이 동굴 범위 밖에 있을 때 다시 탐색
                 continue
-            if visited[ny][nx]:                 #방문 한적 있는 부분일 때 다시 탐색
-                continue
             if cave[ny][nx] == '.':             #동굴에 x가(미네랄이) 없는 부분일 때 다시 탐색
                 continue
+            if visited[ny][nx]:                 #방문 한적 있는 부분일 때 다시 탐색
+                continue
 
-            q.append((nx, ny))    #q에 미네랄이 있는 위치 입력
+
+            q.append((ny, nx))    #q에 미네랄이 있는 위치 입력
             visited[ny][nx] = True  #cave 탐색 후 미네랄이 있는 곳 False -> True
 
     return visited  #방문 기록이 담긴 visited 리스트 반환
@@ -136,9 +129,30 @@ def minYList(visited):
                 break
     return minY_list
 
+#어디까지 내려갈 수 있는지 확인
+def fallHeight(minY_list):   #minY_list : [-1, -1, 5, 4, 5, 5, -1, -1]
+    min_diff = max_r + 1     #max_r : 100
+    for x, min_y in enumerate(minY_list):   #x : 리스트 인덱스 확인, min_y : 인덱스 x에 따른 요소
+        if min_y == -1 :    #아래로 내릴 클러스터 외 다른 부분은 확인하지 않는다
+            continue        
 
-def fallHeight(minY_list):
-    min_diff = max_r + 1
+        for y in range(min_y+1, r):  #아래로 내릴 클러스터의 밑 부분들을 확인
+            if cave[y][x] == 'x':                        #그 밑에 미네랄이 있으면 내려갈 수 없으니까 멈추기
+                min_diff = min(min_diff, y - min_y -1)   
+                break
+            if y == r-1:
+                min_diff = min(min_diff, y - min_y)
+    return min_diff     #내려갈 수 있는 정도 반환
+
+
+#클러스터를 내려주기
+def fall(fall_h, visited):                  #h: 내려갈 수 있는 정도, visited: 클러스터 열의 맨 아래부분에 있는 미네랄들
+    for y in range(r-1, -1, -1):
+        for x in range(c):
+            if not visited[y][x]:      #visited[y][x]가 False값을 가지면
+                continue               #다시 반복
+            cave[y][x] = '.'
+            cave[y+fall_h][x] = 'x'         #내려갈 수 있는 곳까지 클러스터를 내린다
     
 
 
@@ -148,13 +162,13 @@ for h in height:   #[6,6,4,3,1]
     h = r-h
     target_x, target_y = -1, -1
     if turn == chang:
-        target_x, target_y = throwStickLeft(h)
+        target_y, target_x = throwStickLeft(h)
         turn = sang
     else:
-        target_x, target_y = throwStickRight(h)
+        target_y, target_x = throwStickRight(h)
         turn = chang
 
-    if (target_x, target_y) == (-1, -1):    #cave에서 해당되는 좌표를 얻지 못했을 때 다시 던지기 시도
+    if (target_y, target_x) == (-1, -1):    #cave에서 해당되는 좌표를 얻지 못했을 때 다시 던지기 시도
         continue
     #[['.', '.', '.', '.', '.', '.', '.', '.'], 
     # ['.', '.', '.', '.', '.', '.', '.', '.'], 
@@ -179,12 +193,25 @@ for h in height:   #[6,6,4,3,1]
         if r-1 in minY_list:    #mivY_list안에 r-1이 있으면 True
             continue
 
-        fall_height = fallHeight(minY_list)
-        fall(fall_height, visited)
-        break
+        fall_height = fallHeight(minY_list) #내릴 클러스터가 최대한 내려갈 수 있는 정도를 fall_height에 넣기
+        fall(fall_height, visited)          #최대로 내려갈 수 있는 높이와, 내릴 클러스터의 가장 아래부분을 모은 visited를 전달
+        break                               
 
-print(visited)
-print(minY_list)
+#클러스터를 내린 결과를 출력
+for row in range(r):
+    for colunm in range(c):
+        print(cave[row][colunm], end="")
+    print()
+#------------------------------------------------------------------------
+
+#[['.', '.', '.', '.', '.', '.', '.', '.'], 
+# ['.', '.', '.', '.', '.', '.', '.', '.'], 
+# ['.', '.', '.', '.', '.', 'x', '.', '.'], 
+# ['.', '.', '.', 'x', 'x', 'x', '.', '.'], 
+# ['.', '.', '.', 'x', 'x', '.', '.', '.'], 
+# ['.', '.', 'x', '.', 'x', 'x', '.', '.'], 
+# ['.', '.', 'x', '.', '.', '.', 'x', '.'], 
+# ['.', '.', 'x', 'x', '.', '.', 'x', '.']]
 
 #[[False, False, False, False, False, False, False, False], 
 # [False, False, False, False, False, False, False, False], 
@@ -207,8 +234,134 @@ print(minY_list)
 #[-1, -1, 5, 4, 5, 5, -1, -1]
 
 
+import sys
+from collections import deque
+CHANG_YOUNG, SANG_GEUN = 0, 1
+DIRS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+MAX_R = 100
 
+R, C = map(int, sys.stdin.readline().rstrip().split())
+cave = []
+for _ in range(R):
+    cave.append(list(sys.stdin.readline().rstrip()))
+    # 입력 받으면서 뭔가 할 수 있지 않을까? 
+N = int(sys.stdin.readline().rstrip())
+H = list(map(int, sys.stdin.readline().rstrip().split()))
 
+# 매번 BFS 돌리는 건 에바고
+# 매번 없앨 수 있는 미네랄은 한개
+# 그 한개의 상하좌우에 미네랄이 있으면, 그 미네랄 섬이 떨어질 수도 있는 후보.
+# 각 미네랄 섬에 대해서 BFS를 했을 때, 섬 중 최저 y 좌표를 구한다. 
+# y 좌표가 0이면 break, 0이 아니면 그만큼 떨굼.
+# https://chldkato.tistory.com/62
+
+def throw_stick_from_left(h):
+    for x in range(C):
+        if cave[h][x] == 'x':
+            cave[h][x] = '.'
+            return h, x
+    return -1, -1
+
+def throw_stick_from_right(h):
+    for x in range(C-1, -1, -1):
+        if cave[h][x] == 'x':
+            cave[h][x] = '.'
+            return h, x
+    return -1, -1
+
+def print_cave():
+    for row in cave:
+        print(*row, sep='')
+
+def get_cluster_visited(start_y, start_x):
+    visited = [[False]*C for _ in range(R)]
+    q = deque([(start_y, start_x)])
+    visited[start_y][start_x] = True
+    while q:
+        y, x = q.popleft()
+        for dy, dx in DIRS:
+            ny, nx = y+dy, x+dx
+            if not (0 <= ny < R and 0 <= nx < C):
+                continue
+            if cave[ny][nx] == '.':
+                continue
+            if visited[ny][nx]:
+                continue
+            q.append((ny, nx))
+            visited[ny][nx] = True
+    return visited
+
+def get_min_y_list(visited):
+    min_y_list = [-1]*C
+    for x in range(C):
+        for y in range(R-1, -1, -1):
+            if visited[y][x]:
+                min_y_list[x] = y
+                break
+    return min_y_list
+            
+def get_fall_height(min_y_list):
+    min_diff = MAX_R + 1
+    for x, min_y in enumerate(min_y_list):
+        if min_y == -1: # 엣지 케이스
+            continue
+        for y in range(min_y + 1, R):
+            if cave[y][x] == 'x':
+                min_diff = min(min_diff, y - min_y - 1) # 처음 만난 미네랄이 5, min_y가 3이라면 1칸 내릴 수 있음
+                break
+            if y == R-1: # 바닥
+                min_diff = min(min_diff, y - min_y) # 바닥이 5, min_y가 3이라면 2칸 내릴 수 있음
+                # break # 어차피 마지막
+    return min_diff
+
+def fall(h, visited):
+    # 아래서부터 내린다.
+    for y in range(R-1, -1, -1):
+        for x in range(C):
+            if not visited[y][x]:
+                continue
+            cave[y][x] = '.'
+            cave[y+h][x] = 'x'
+
+turn = CHANG_YOUNG
+for h in H:
+    # 1 -> R-1, 2 -> R-2, ..., R -> 0
+    h = R-h
+    mineral_y, mineral_x = -1, -1
+    if turn == CHANG_YOUNG:
+        mineral_y, mineral_x = throw_stick_from_left(h)
+        turn = SANG_GEUN
+    else:
+        mineral_y, mineral_x = throw_stick_from_right(h)
+        turn = CHANG_YOUNG
+    if (mineral_y, mineral_x) == (-1, -1): # 깨진 미네랄이 없을 때
+        continue
+    # 후보는, 없어진 미네랄의 상하좌우
+    for dy, dx in DIRS:
+        ny, nx = mineral_y + dy, mineral_x + dx
+        if not (0 <= ny < R and 0 <= nx < C):
+            continue
+        if cave[ny][nx] == '.':
+            continue
+        # 주의. 클러스터가 떨어질 때, **그 클러스터 각 열의 맨 아래 부분 중 하나**가 바닥 또는 미네랄 위로 떨어지는 입력만 주어진다!
+        visited = get_cluster_visited(ny, nx)
+        
+        # 오답.
+        # 맨 아래 층에 있던 애들만 닿는 건 아니다!!!
+        # **각 열**의 맨 아래 요소에 대해서 다 체크해야 함. 
+        min_y_list = get_min_y_list(visited)
+        
+        if R-1 in min_y_list: # 바닥에 닿은 맨 아래 요소가 있다면
+            continue
+                    
+        # 최저점이 바닥이 아닌 경우 == 현재 떠 있는 경우
+        fall_height = get_fall_height(min_y_list)
+        fall(fall_height, visited)
+
+        # 주의. 두 개 이상의 클러스터가 동시에 떨어지는 경우는 없다. 
+        break
+
+print_cave()
 
 
 
@@ -332,49 +485,6 @@ print(minY_list)
 # ['.', '.', 'x', 'x', '.', '.', 'x', '.']]
 
 #10
-
-
-
-
-
-
-
-
-
-def bfs(matrix, sx, sy):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, 1, -1]
-    block = [[sx, sy]]
-    q = deque([[sx, sy]])
-    matrix[sx][sy] = '.'
-
-    if sx == r-1:
-        down = False
-    else:
-        down = True
-
-    while q:
-        sx, sy = q.popleft()
-
-        for i in range(4):
-            nx, ny = sx+dx[i], sy+dy[i]
-            if (0 <= nx < r) and (0 <= ny < c):
-                matrix[nx][ny] = 'x'
-
-            if nx == r-1:
-                down = False
-            else:
-                down = True
-            
-            block.append([nx, ny])
-            q.append([nx, ny])
-            matrix[nx, ny] = '.'
-
-    return block, down
-
-
-
-
 
 
 # visited
